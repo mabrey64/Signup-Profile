012cc4fbe1eb13b3f34bd251b635e168d0e0fd53
mabreydn <mabreydn@mail.uc.edu>
2025-06-27 13:17:46 -0400

    Created Class
    Started the project with creating a profile class and allow for entry using the profile's params.

---
+++ Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signup_Profile
+{
+    internal class Profile
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public Profile(string username, string email, string password, string confirmPassword)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+        }
+    }
+}
